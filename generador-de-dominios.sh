3d48ffde0ca8062d0e02d16595672ed66b3770ad
Matías Jesús Gonzalez <81663683+matijego@users.noreply.github.com>
2022-06-08 11:56:20 -0300

    Update generador-de-dominios.sh

--- generador-de-dominios.sh
+++ generador-de-dominios.sh
@@ -17,6 +17,9 @@ sed -i s/EXAMPLE-IP/$IP/g /etc/nginx/sites-available/$DOMINIO
 #Crea una referencia en la carpeta de sitios activados en nginx
 ln -s /etc/nginx/sites-available/$DOMINIO /etc/nginx/sites-enabled
 
+#Instala el certificado SSL con certbot
+sudo certbot --nginx -d $DOMINIO
+
 #Reinicia nginx para que se apliquen los cambios
 systemctl restart nginx.service
 
